c173128ef094b38f0fa73e430ac8c7ad62058772
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-03-04 15:39:10 +0200

    build components

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="./assets/logo.png" width="80">
+    <app-quote-grid :quote="quotes"></app-quote-grid>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import QuoteGrid from './components/QuoteGrid'
 export default {
   name: 'app',
+  data () {
+    return {
+      quotes: [
+        'Coding is coding no matter the language'
+      ],
+      maxQuotes: 10
+    }
+  },
   components: {
-    HelloWorld
+    appQuoteGrid: QuoteGrid
   }
 }
 </script>
 
 <style>
+@import url('https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.css');
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
