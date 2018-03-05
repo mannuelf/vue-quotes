e5c57538840e6788df97b455ae76cfda1cb22f15
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-03-05 17:37:13 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,40 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png" width="80">
+  <div id="app" class="container">
     <app-quote-grid :quote="quotes"></app-quote-grid>
   </div>
 </template>
 
 <script>
-import QuoteGrid from './components/QuoteGrid'
+import QuoteGrid from './components/QuoteGrid.vue'
+import NewQuote from './components/NewQuote.vue'
+import Header from './components/Header.vue'
+
 export default {
   name: 'app',
   data () {
     return {
       quotes: [
-        'Coding is coding no matter the language'
+        'Coding is coding no matter the language',
+        'Tomorrow is another day'
       ],
       maxQuotes: 10
     }
   },
+  methods: {
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert('please delete Quotes first')
+      }
+      this.quotes.push(quote)
+    }
+  },
   components: {
-    appQuoteGrid: QuoteGrid
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  },
+  created () {
+    console.log('app init');
   }
 }
 </script>
